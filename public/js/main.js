bbb507c857a846ed3a73db976c86d8d8c02f49e1
Maksym_Labazov <maksym_labazov@epam.com>
2016-08-23 08:19:22 +0200

    added jquery

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,23 @@ function ItemTypeService() {
                 displayType(data);
             }
         });
+    };
+    this.loadItemsByType = function(type) {
+        $.ajax({
+            url : "../items/" + type,
+            type : 'get',
+            success : function(data) {
+                displayItems(data);
+            }
+        });
     }
 }
 
 function displayType(types) {
     console.log(types);
+}
+function displayItems(items) {
+    console.log(items);
 }
 
 $(document).ready(function() {
